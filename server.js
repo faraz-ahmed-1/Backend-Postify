15961dfbc21773224b4881a6d16087bf9d3fd588
Faraz Ahmed <farazfatahsoomro@gmail.com>
2025-12-31 22:01:42 +0500

    fixed

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   }
 });
 
